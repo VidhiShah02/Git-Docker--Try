3d039d5f60e9934ca3f3d6ecb2bd933a51ca6286
unknown <vidhis843@gmail.com>
2023-05-12 15:13:28 +0530

    redis changes-1

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import socket
 
 
 
-redis = Redis(host='redis', port=6379, db=0, )
+redis = Redis(host='10.16.4.139', port=6379, db=0, )
 app = Flask(__name__)
 
 @app.route('/')
@@ -14,28 +14,6 @@ def index():
     redis.set('mykey', 'Hello from Python!')
     return render_template('./app.html')
  
-
-#value = redis.get('mykey')
-#print(value)
-
-#redis.zadd('vehicles', {'car' : 0})
-#redis.zadd('vehicles', {'bike' : 0})
-#vehicles = redis.zrange('vehicles', 0, -1)
-    # return jsonify({'data': "shubham"})
-  
-  
-# on the terminal type: curl http://127.0.0.1:5000/
-# returns hello world when we use GET.
-# returns the data that we send when we use POST.
-# @app.route('/', methods = ['GET', 'POST'])    
-# def home():
-#     if(request.method == 'GET'):
-  
-#         data = "hello world"
-#         return jsonify({'data': data})
-    
-
-
 if __name__ == '__main__':
    port = int(os.environ.get("PORT", 9595))
    app.run(debug=True,host='0.0.0.0',port=port)
