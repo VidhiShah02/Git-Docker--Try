41c6db08641cc80362cd9e89b165e1cdb0f7f153
unknown <vidhis843@gmail.com>
2023-05-22 12:23:57 +0530

    Using Helm Chart

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 from flask import Flask, render_template, jsonify, request
-#from redis import Redis, RedisError
+from redis import Redis, RedisError
 import os 
 import socket
 
 
 
-#redis = Redis(host='10.16.4.139', port=6379, db=0, )
+redis = Redis(host='10.16.4.139', port=6379, db=0, )
 app = Flask(__name__)
 #redis.set('mykey', 'Hello from Python!')
 @app.route('/')
 def index():
     #print('Try')
-    #redis.set('mykey', 'Hello from Python!')
+    redis.set('mykey', 'Hello from Python!')
     return render_template('./app.html')
  
 if __name__ == '__main__':
